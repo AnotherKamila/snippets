1ae0e9695d740fe3970b97ee049b60d56f2e943f
Kamila Součková <kamila@ksp.sk>
2018-03-12 11:12:03 +0100

    Update python-tricks.py

--- python-tricks.py
+++ python-tricks.py
@@ -5,3 +5,15 @@ def code_i_want_to_debug(a, b):
     
     from ptpython.repl import embed
     embed(globals(), locals())  # has access to all the variables
+
+    
+# Use a README.md but convert to rst when uploading to PyPI
+
+# put this in setup.py:
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print('warning: pypandoc module not found, could not convert Markdown to RST')
+    read_md = lambda f: open(f, 'r').read()
+# then use e.g. `long_description=read_md('README.md')`
